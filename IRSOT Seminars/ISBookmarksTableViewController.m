de9cefce8df3a4e25794e2f1c9f0c2cae3e9dbe8
Bob Ershov <raveman@alderaan.office.irsot.net>
2012-08-17 20:06:48 +0400

    Storyboard: changes to Settings View, added use iCloud switch

--- IRSOT Seminars/ISBookmarksTableViewController.m
+++ IRSOT Seminars/ISBookmarksTableViewController.m
@@ -73,16 +73,14 @@
     return cell;
 }
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -94,7 +92,7 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.

a1eb3e520b1d162ef7f86dd6a63ce204f7532d64
Bob Ershov <raveman@raveman.ru>
2013-02-04 18:46:51 +0400

    Added standard web navigation toolbar to seminar web view Added ability to open in safari when browsing seminar web page Fixed localization errors

--- IRSOT Seminars/ISWebviewViewController.m
+++ IRSOT Seminars/ISWebviewViewController.m
@@ -9,10 +9,17 @@
 #import "SVProgressHUD/SVProgressHUD.h"
 #import "ISWebviewViewController.h"
 
-@interface ISWebviewViewController () <UIWebViewDelegate>
+@interface ISWebviewViewController () <UIWebViewDelegate, UIActionSheetDelegate>
 //@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
 @property (weak, nonatomic) IBOutlet UIWebView *webview;
 @property (weak, nonatomic) IBOutlet UINavigationBar *modalNavigationBar;
+@property (weak, nonatomic) IBOutlet UIToolbar *modalWebToolBar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *actionButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *reloadButton;
+
+@property (weak, nonatomic) UIActionSheet *actionSheet;
 
 @end
 
@@ -20,6 +27,13 @@
 //@synthesize scrollview;
 @synthesize webview;
 @synthesize modalNavigationBar;
+@synthesize modalWebToolBar;
+@synthesize backButton;
+@synthesize forwardButton;
+@synthesize reloadButton;
+@synthesize actionButton;
+
+@synthesize actionSheet = _actionSheet;
 
 @synthesize url = _url;
 
@@ -35,13 +49,19 @@
     
     if (self.navigationController) {
         self.modalNavigationBar.hidden = YES;
+        self.modalWebToolBar.hidden = YES;
 
     } else {
         self.modalNavigationBar.topItem.title = self.webviewTitle;
+        self.modalWebToolBar.hidden = NO;
         CGRect webviewFrame = self.webview.frame;
         webviewFrame.size.height -= self.modalNavigationBar.frame.size.height;
         webviewFrame.origin.y = self.modalNavigationBar.frame.size.height;
         self.webview.frame = webviewFrame;
+        self.backButton.enabled = NO;
+        self.forwardButton.enabled = NO;
+        self.actionButton.enabled = NO;
+        self.reloadButton.enabled = NO;
     }
     
 	// Do any additional setup after loading the view.
@@ -53,6 +73,11 @@
 //    [self setScrollview:nil];
     [self setModalNavigationBar:nil];
 
+    [self setModalWebToolBar:nil];
+    [self setBackButton:nil];
+    [self setForwardButton:nil];
+    [self setActionButton:nil];
+    [self setReloadButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.    
 }
@@ -60,6 +85,7 @@
 - (void)viewWillDisappear:(BOOL)animated
 {
     [SVProgressHUD dismiss];
+
     [super viewWillDisappear:animated];
 }
 
@@ -86,13 +112,58 @@
 {
     [SVProgressHUD dismiss];
     
+    if (webview.canGoBack) self.backButton.enabled = YES;
+        else self.backButton.enabled = NO;
+    if (webview.canGoForward) self.forwardButton.enabled = YES;
+        else self.forwardButton.enabled = NO;
+    
+    self.reloadButton.enabled = YES;
+    self.actionButton.enabled = YES;
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
 {
-    [SVProgressHUD showErrorWithStatus:@"Ошибка загрузки:-("];
+   
+    [SVProgressHUD showErrorWithStatus:[NSString stringWithFormat:@"%@", [error localizedDescription]]];
     
 }
 
+#pragma mark - web buttons methods
+
+- (IBAction)backButtonPressed:(UIBarButtonItem *)sender {
+    [self.webview goBack];
+}
+- (IBAction)forwardButtonPressed:(UIBarButtonItem *)sender {
+    [self.webview goForward];
+}
+- (IBAction)reloadButtonPressed:(UIBarButtonItem *)sender {
+    [self.webview reload];
+}
+
+#pragma mark - Button Actions
+- (IBAction)share:(UIBarButtonItem *)sender {
+    if (self.actionSheet) {
+        // do nothing
+    } else {
+        NSString *openInSafariButton = NSLocalizedString(@"Открыть в Safari", @"Open in Safari");
+        
+        UIActionSheet *actionSheet = nil;
+        
+        actionSheet = [[UIActionSheet alloc] initWithTitle:self.webview.request.URL.absoluteString delegate:self cancelButtonTitle:NSLocalizedString(@"Отмена", @"Cancel") destructiveButtonTitle:nil otherButtonTitles:openInSafariButton,  nil];
+        [actionSheet showFromBarButtonItem:sender animated:YES];
+        self.actionSheet = actionSheet;
+    }
+
+}
+
+#pragma mark - UIActionSheetDelegate
+
+- (void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == [actionSheet destructiveButtonIndex]) {
+    } else if (buttonIndex == 0) {
+        //open in Safari
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.webview.request.URL.absoluteString]];
+    }
+}
 
 @end

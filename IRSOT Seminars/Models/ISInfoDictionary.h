035683d158a9e8f7bf098a897504b696267e84a1
Bob Ershov <raveman@raveman.ru>
2013-02-14 23:40:14 +0400

    Added new class ISInfoDictionary as substitution of Info Links.plist which is with controlled sorting

---
+++ IRSOT Seminars/Models/ISInfoDictionary.h
@@ -0,0 +1,16 @@
+//
+//  ISInfoDictionary.h
+//  IRSOT Seminars
+//
+//  Created by Bob Ershov on 14.02.13.
+//  Copyright (c) 2013 IRSOT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISInfoDictionary : NSObject
+
++ (NSArray *) infoArray; // info array
++ (NSDictionary *) info; // info dictionary
+
+@end

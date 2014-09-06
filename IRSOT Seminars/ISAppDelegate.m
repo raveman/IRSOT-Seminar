3b7f19e5ddaa8b51b15a3cc732bd6496928c6330
Bob Ershov <raveman@raveman.ru>
2014-09-06 22:26:19 +0400

    Added ios 7 app icons

--- IRSOT Seminars/ISAppDelegate.m
+++ IRSOT Seminars/ISAppDelegate.m
@@ -8,8 +8,6 @@
 
 #import "ISAppDelegate.h"
 
-//#import "LocalyticsSession.h"
-
 #import "ISMainPageViewController.h"
 #import "ISBookmarksTableViewController.h"
 #import "ISLectorListTableViewController.h"
@@ -62,39 +60,31 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 
-//    [[LocalyticsSession sharedLocalyticsSession] close];
-//    [[LocalyticsSession sharedLocalyticsSession] upload];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-//    [[LocalyticsSession sharedLocalyticsSession] close];
-//    [[LocalyticsSession sharedLocalyticsSession] upload];
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-//    [[LocalyticsSession sharedLocalyticsSession] resume];
-//    [[LocalyticsSession sharedLocalyticsSession] upload];
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-//    [[LocalyticsSession sharedLocalyticsSession] resume];
-//    [[LocalyticsSession sharedLocalyticsSession] upload];
+
 
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Saves changes in the application's managed object context before the application terminates.
-//    [[LocalyticsSession sharedLocalyticsSession] close];
-//    [[LocalyticsSession sharedLocalyticsSession] upload];
     
     [self saveContext];
     [self.kvStore synchronize];
